0d9213b0bdad57482f32c5041aaca75a3118c78f
Duro375 <denisdjuris@gmail.com>
2026-05-12 21:30:25 +0200

    Dodati data setovi

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Client
 {
@@ -10,6 +11,19 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string[] paths = path.Split(Path.DirectorySeparatorChar);
+            string newPath = "";
+            for (int i = 0; i < paths.Length - 4; i++)
+            {
+                newPath += paths[i] + Path.DirectorySeparatorChar;
+            }
+            newPath += "Datasets" + Path.DirectorySeparatorChar + "BMW iX xDrive50" + Path.DirectorySeparatorChar + "Charging_Profile.csv";
+            Console.WriteLine(newPath);
+            TextReader textReader = new StreamReader(newPath);
+            string abc = textReader.ReadLine();
+            abc = textReader.ReadLine();
+            Console.WriteLine(abc);
         }
     }
 }
