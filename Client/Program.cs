757b39ee2dfe84ac76e5243a2ab7f79b1f63bc83
Denis Đuriš <denisdjuris@gmail.com>
2026-05-16 16:30:40 +0200

    Dodat Meni u klijentu, i dodata je klasa FolderManipulation za izlistavanje foldera/vozila

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Common;
 
 namespace Client
 {
@@ -11,19 +12,35 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            string path = AppDomain.CurrentDomain.BaseDirectory;
-            string[] paths = path.Split(Path.DirectorySeparatorChar);
-            string newPath = "";
-            for (int i = 0; i < paths.Length - 4; i++)
+            string path = Menu();
+            Console.WriteLine(path);
+        }
+
+        public static string Menu()
+        {
+            FolderManipulation folders = new FolderManipulation();
+            Console.WriteLine("Dobar dan, molimo vas odaberite jedno od sledecih vozila (1 - 12):  \n");
+            int broj = 0;
+            while(true)
             {
-                newPath += paths[i] + Path.DirectorySeparatorChar;
+                folders.PrintAvailableDirectories();
+                Console.WriteLine("Unesite broj vozila (1-12): ");
+                try
+                {
+                    broj = int.Parse(Console.ReadLine());
+                    if (broj <= 12 && broj >= 1)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("\nNeispravan unos, molimo unesite ispravan broj vozila!\n\n");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\nNeispravan unos, molimo unesite broj vozila (1-12)\n\n");
+                }
             }
-            newPath += "Datasets" + Path.DirectorySeparatorChar + "BMW iX xDrive50" + Path.DirectorySeparatorChar + "Charging_Profile.csv";
-            Console.WriteLine(newPath);
-            TextReader textReader = new StreamReader(newPath);
-            string abc = textReader.ReadLine();
-            abc = textReader.ReadLine();
-            Console.WriteLine(abc);
+            string odabranFolder = folders.ChooseDirectory(broj);
+            return odabranFolder;
         }
     }
 }
