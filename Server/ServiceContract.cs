bb5d85bf5019fae1b764ac87e8a3b07a68f8cfdb
Danko Marković <dankomarkovic20042@gmail.com>
2026-05-16 19:33:48 +0200

    Male izmene

--- Server/ServiceContract.cs
+++ Server/ServiceContract.cs
@@ -27,30 +27,31 @@ namespace Server
         {
         }
 
-        //3. zadatak: Validacija i fault message
+        //3. zadatak: Validacija i fault poruka
         private string Validate(DataContract data)
         {
             if(data.TimeStamp <= DateTime.MinValue)
                 return "Invalid Timestapm";
             if(data.Voltage_RMS_Avg < 0)
-                return string.Format("Invalid Voltage_RMS_Avg: {0}", data.Voltage_RMS_Avg);
+                return string.Format("Nevalidan podatak Voltage_RMS_Avg: {0}", data.Voltage_RMS_Avg);
             if (data.Current_RMS_Avg < 0)
-                return string.Format("Invalid Current_RMS_Avg: {0}", data.Current_RMS_Avg); 
+                return string.Format("Nevalidan podatak Current_RMS_Avg: {0}", data.Current_RMS_Avg); 
             if(data.Real_Power_Avg < 0)
-                return string.Format("Invalid Real_Power_Avg: {0}", data.Real_Power_Avg); 
-            if (data.Reactive_Power_Avg < 0)
-                return string.Format("Invalid Reactive_Power_Avg: {0}", data.Reactive_Power_Avg);
+                return string.Format("Nevalidan podatak Real_Power_Avg: {0}", data.Real_Power_Avg); 
+            if (data.Reactive_Power_Avg > 0)
+                return string.Format("Nevalidan podatak Reactive_Power_Avg: {0}", data.Reactive_Power_Avg);
             if (data.Apparent_Power_Avg < 0)
-                return string.Format("Invalid Apparent_Power_Avg: {0}", data.Apparent_Power_Avg);
+                return string.Format("Nevalidan podatak Apparent_Power_Avg: {0}", data.Apparent_Power_Avg);
             if (data.Frequency_Avg < 0)
-                return string.Format("Invalid Frequency_Avg: {0}", data.Frequency_Avg);
+                return string.Format("Nevalidan podatak Frequency_Avg: {0}", data.Frequency_Avg);
             return null;
         }
 
         private void SendFaultMessage(string message)
         {
             throw new FaultException<CustomException>(
-                new CustomException(message));
+                new CustomException(message),
+                new FaultReason(message));
         }
     }
 }
