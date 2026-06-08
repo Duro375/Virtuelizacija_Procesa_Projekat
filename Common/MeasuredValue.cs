5d700b4e0c8510fe9ce28edf98b4d451cbd4b24d
Duro375 <denisdjuris@gmail.com>
2026-06-08 19:59:22 +0200

    Dodata MeasuredValue klasa

---
+++ Common/MeasuredValue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [DataContract]
+    public class MeasuredValue
+    {
+        [DataMember]
+        public double MinValue { get; set; }
+        public double AvgValue { get; set; }
+        public double MaxValue { get; set; }
+        public MeasuredValue() { }
+        public MeasuredValue(double minValue, double avgValue, double maxValue)
+        {
+            MinValue = minValue;
+            AvgValue = avgValue;
+            MaxValue = maxValue;
+        }
+    }
+}
